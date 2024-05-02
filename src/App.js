efeae279ed67052bb7d6b988499bd365abd040f8
Juanequex <ramon.g.uz37@gmail.com>
2024-05-02 15:52:40 -0600

    [setup] codebase off transfer app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// import logo from "../src/assets/sinayuca.png"
+import Footer from "./components/Footer";
+import Inputs from "./components/inputs"
+import Whatapp from './components/Whatapp';
+
+function App() {
+  return (
+    <div className="main-container">
+      {/* <img src={logo} alt='logo' className='main-container__logo' /> */}
+      <Inputs />
+      <Whatapp />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Data.js
@@ -0,0 +1,5 @@
+export const Data = [
+  {id: 1,title: "Cuenta Bancaria", value: "444444444444444", buttonText: "Copiar Cuenta", campo: "cuenta" },
+  {id: 2,title: "Nombre de la cuenta", value: "Nombre de la cuenta", buttonText: "Copiar Nombre", campo: "nombre" },
+  {id: 3,title: "Banco", value: "Banco", buttonText: "Copiar Banco", campo: "banco" },
+]
